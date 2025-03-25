0233109e0b4f40e6d99a8c8dede170c72d844501
Will Halfpenny <30996474+will-0@users.noreply.github.com>
2025-03-25 20:39:05 +0000

    layout

--- src/mobile/components/custom/PatientCard.tsx
+++ src/mobile/components/custom/PatientCard.tsx
@@ -4,13 +4,13 @@ import { Patient } from '@/state/api';
 
 export default function PatientCard(index: number, patient: Patient, isSuccess: boolean) {
     return (
-        <Box key={index} className='w-full p-4 bg-secondary-300 rounded-lg'>
+        <Box key={index} className='p-4 bg-secondary-200 rounded-lg shadow-sm shadow-secondary-500'>
             <Heading size='lg' className='mb-2'>{patient.mrn}</Heading>
             <Box className='flex flex-row justify-between items-center'>
                 <Heading size='md'>Ratings</Heading>
                 <Box className='flex flex-row'>
                     {isSuccess && patient.ratings.map((rating, index) => (
-                        <Box key={index} className='bg-gray-200 rounded-full h-6 w-6 flex items-center justify-center mx-1'>
+                        <Box key={index} className='bg-secondary-100 rounded-full h-6 w-6 flex items-center justify-center mx-1'>
                             <Heading size='sm'>{rating.rating}</Heading>
                         </Box>
                     ))}
