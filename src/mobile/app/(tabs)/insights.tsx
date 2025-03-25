35e3bab0545114531e07742e1c645f3503894618
Will Halfpenny <30996474+will-0@users.noreply.github.com>
2025-03-25 21:10:45 +0000

    Label insights as in progress

--- src/mobile/app/(tabs)/insights.tsx
+++ src/mobile/app/(tabs)/insights.tsx
@@ -2,13 +2,13 @@ import BaseView from '@/components/BaseView';
 import PatientForm from '@/components/custom/PatientForm';
 import { Box } from '@/components/ui/box';
 import { Heading } from '@/components/ui/heading';
+import { Text } from '@/components/ui/text';
 
 export default function HomeScreen() {
   return (
     <BaseView>
       <Box className='flex-1 justify-center items-center w-full'>
-        <Heading size="2xl">Welcome, Will</Heading>
-        <PatientForm  />
+        <Heading>In Progress...</Heading>
       </Box>
     </BaseView>
   );
