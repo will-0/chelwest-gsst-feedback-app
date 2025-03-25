1db501437f7ac1745a00f092545d1386ee84d552
Will Halfpenny <30996474+will-0@users.noreply.github.com>
2025-03-25 20:23:27 +0000

    Add search bar to ratings

--- src/mobile/app/(tabs)/dashboard.tsx
+++ src/mobile/app/(tabs)/dashboard.tsx
@@ -6,6 +6,7 @@ import { SearchIcon } from '@/components/ui/icon';
 import { Input, InputField, InputIcon, InputSlot } from '@/components/ui/input';
 import { Select } from '@/components/ui/select';
 import { useGetPatientsQuery } from '@/state/api';
+import { useState } from 'react';
 import { ScrollView } from 'react-native';
 import { SearchBar } from 'react-native-screens';
 
@@ -16,14 +17,29 @@ export default function HomeScreen() {
     isSuccess
   } = useGetPatientsQuery();
 
+  const [search, setSearch] = useState('');
+
   return (
     <BaseView>
-      <Box className='flex flex-1 flex-col justify-start items-center w-full p-8'>
+      <Box className='flex flex-1 flex-col justify-start items-center w-full p-8 gap-4'>
         <Heading size="2xl">Ratings</Heading>
+        <Input>
+          <InputField value={search} onChangeText={setSearch} />
+          <InputSlot className='pr-4'>
+            <InputIcon as={SearchIcon} />
+          </InputSlot>
+        </Input>
         <ScrollView className='w-full'>
-          <Box className='flex flex-col gap-4 w-full pl-0 pr-0 pt-4 pb-4'>
+          <Box className='flex flex-col gap-4 w-full'>
             {
-              patients?.map((patient, index) => (
+              patients?.filter((value) => {
+                if (search === '') {
+                  return value;
+                }
+                else if (value.mrn.toLowerCase().startsWith(search.toLowerCase())) {
+                  return value;
+                }
+              }).map((patient, index) => (
                 <Box key={index} className='w-full p-4 bg-secondary-300 rounded-lg'>
                   <Heading size='lg' className='mb-2'>{patient.mrn}</Heading>
                   <Box className='flex flex-row justify-between items-center'>
