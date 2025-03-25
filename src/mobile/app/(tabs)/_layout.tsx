4f1b3818b2af4ffc3409ee2027baab74b0bece52
Will Halfpenny <30996474+will-0@users.noreply.github.com>
2025-03-25 20:27:30 +0000

    set add as default

--- src/mobile/app/(tabs)/_layout.tsx
+++ src/mobile/app/(tabs)/_layout.tsx
@@ -15,7 +15,7 @@ export default function TabLayout() {
 
   return (
     <Tabs
-      initialRouteName="dashboard"
+      initialRouteName="index"
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
