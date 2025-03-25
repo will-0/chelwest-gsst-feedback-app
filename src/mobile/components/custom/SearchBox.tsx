0a38ed644671841101c6a7263bdfe643ed7d3eec
Will Halfpenny <30996474+will-0@users.noreply.github.com>
2025-03-25 21:08:44 +0000

    refactr app serach box

--- src/mobile/components/custom/SearchBox.tsx
+++ src/mobile/components/custom/SearchBox.tsx
@@ -2,14 +2,13 @@ import { Box } from '@/components/ui/box';
 import { SearchIcon } from '@/components/ui/icon';
 import { Input, InputField, InputIcon, InputSlot } from '@/components/ui/input';
 
-export default function AppSearchBox({search, setSearch}: {search: string, setSearch: (value: string) => void}) {
+export default function AppSearchBox({ search, setSearch }: { search: string, setSearch: (value: string) => void }) {
     return <Box className='w-full p-2'>
-      <Input>
-        <InputField value={search} onChangeText={setSearch} />
-        <InputSlot className='pr-4'>
-          <InputIcon as={SearchIcon} />
-        </InputSlot>
-      </Input>
+        <Input>
+            <InputField value={search} onChangeText={setSearch} />
+            <InputSlot className='pr-4'>
+                <InputIcon as={SearchIcon} />
+            </InputSlot>
+        </Input>
     </Box>;
-  }
-  
+}
