9f2c7af8f6751dc07a0387a3ed3298abd1beea5c
Will Halfpenny <30996474+will-0@users.noreply.github.com>
2025-03-27 23:48:28 +0000

    add auth endpoints

--- src/mobile/constants/Endpoints.ts
+++ src/mobile/constants/Endpoints.ts
@@ -1,3 +1,3 @@
-export const AUTH_ENDPOINT = ""
-export const TOKEN_ENDPOINT = ""
-export const CLIENT_ID = ""
+export const AUTH_ENDPOINT = "https://login.finomics.app/c1cb7b1b-3c59-4877-98ed-d84aac0c11cc/oauth2/v2.0/authorize"
+export const TOKEN_ENDPOINT = "https://login.finomics.app/c1cb7b1b-3c59-4877-98ed-d84aac0c11cc/oauth2/v2.0/token"
+export const CLIENT_ID = "fd9209cb-c738-4069-ad04-e71175327ab0"
